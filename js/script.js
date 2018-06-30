ad6a74eb0b1622e4d125b5e1acadc624082cf432
aalexandrin <39730107+aalexandrin@users.noreply.github.com>
2018-07-01 02:57:24 +0700

    Оживление блоков, анимация интерактивных элементов

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 var hotelSearchButton = document.querySelector('.hotel-search-button');
-var popup = document.querySelector('.offering-search-form');
+var form = document.querySelector('.offering-form-container');
+var arrivalField = document.querySelector('#arrival-date-field');
+var departureField = document.querySelector('#departure-date-field');
+var adultsNumber = document.querySelector('#adults');
+var childrenNumber = document.querySelector('#children');
+var storedAdults = localStorage.getItem('adults');
+var storedChildren = localStorage.getItem('children');
 
-popup.classList.add('offering-form-invisible');
+if (storedAdults) {
+	adultsNumber.value = storedAdults;
+}
+if (storedChildren) {
+	childrenNumber.value = storedChildren;
+}
+
+form.classList.add('offering-form-invisible');
 hotelSearchButton.addEventListener('click', function (evt) {
 	evt.preventDefault();
-	popup.classList.toggle('offering-form-visible');
+	form.classList.toggle('offering-form-visible');
+});
+
+form.addEventListener('submit', function (evt) {
+	if (!arrivalField.value || !departureField.value || !adultsNumber.value || !childrenNumber.value) {
+		alert('Заполните необходимые поля');
+		evt.preventDefault();
+	}
+	else {
+		localStorage.setItem('adults', adultsNumber.value);
+		localStorage.setItem('children', childrenNumber.value);
+	}
 });
